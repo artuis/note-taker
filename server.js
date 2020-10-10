20ddb98b946dd8e82ed65ccbcd19476dc954285b
Thomas An <tommyan97@msn.com>
2020-10-09 21:46:39 -0700

    moved server script file

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-    fs.readFile(path.join(__dirname, "db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     })
@@ -28,11 +28,11 @@ app.get("/api/notes", function(req, res) {
 app.post("/api/notes", function(req, res) {
     const newNote = req.body;
     newNote.id = uuidv4();
-    fs.readFile(path.join(__dirname, "db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
         if (err) throw err;
         const noteDB = JSON.parse(data);
         noteDB.push(newNote);
-        fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(noteDB), (err, data) => {
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(noteDB), (err, data) => {
             if (err) throw err;
             res.json(newNote);
         })
@@ -40,13 +40,13 @@ app.post("/api/notes", function(req, res) {
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-    fs.readFile(path.join(__dirname, "db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
         if (err) throw err;
         const noteDB = JSON.parse(data);
         for (let i = 0; i < noteDB.length; i++) {
             if (noteDB[i].id === req.params.id) {
                 noteDB.splice(i, 1);
-                fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(noteDB), (err, data) => {
+                fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(noteDB), (err, data) => {
                     if (err) throw err;
                     res.send("200");
                     return;
